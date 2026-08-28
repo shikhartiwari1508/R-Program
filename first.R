9442f2d402f8f5eb64a3cecb752d071d4d1c8d7d
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-08-28 20:12:09 +0530

    First R Program

---
+++ first.R
@@ -0,0 +1,43 @@
+print("Hello world")
+
+# This is an comment...
+
+
+#print multiple values separated by space
+print(c("Apple","Banana","Mango"))
+
+
+#cat() prints output in one line
+cat("Welcome to R Programming...")
+
+
+#cat() automatically puts a space between values
+cat("Hello","Students")
+
+#\n starts a new line 
+cat("Hello\nStudents")
+
+#store a number 
+age <- 20
+cat("Age = ",age)
+
+
+#String insights double quotes
+print("Computer Science")
+
+#quotes and without quotes
+name <- "Shikhar"
+print(name)
+cat(name)
+
+x<-20
+print(x)
+
+
+cat("Hello")
+
+
+x<- 20
+y<- 30
+name<- "Shikhar"
+ls()
