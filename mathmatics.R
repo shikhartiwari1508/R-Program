7c9fc7054873a065c94ca44cde479b1b6428ff53
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-08-28 20:18:34 +0530

    Mathmatics Programs

---
+++ mathmatics.R
@@ -0,0 +1,42 @@
+a=10
+b=20
+a+b
+a-b
+a*b
+a/b
+a^b
+a%%b
+
+# SUM
+a<-25
+b<-85
+print(sum(a,b))
+
+#Find Max number
+
+a<- 25
+b<-85
+print(max(a,b))
+
+
+#Find Min number
+
+a<- 25
+b<-85
+print(min(a,b))
+
+#Largest among three numbers
+
+a<- 25
+b<-85
+c<- 70
+print(max(a,b,c))
+
+
+num <- 1:10
+print(num)
+
+
+
+data <- rep(5,6)
+print(data)
