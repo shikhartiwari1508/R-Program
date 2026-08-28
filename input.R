4cfdb835793d9098a21c1e82e97ade1da15b8abb
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-08-28 20:14:40 +0530

    User Input Program

---
+++ input.R
@@ -0,0 +1,54 @@
+name <- readline("Enter the name :")
+roll <- readline("Enter the Roll No. :")
+print(name)
+print(roll)
+cat(name)
+cat(roll)
+
+
+
+#convert input to numeric using as.numeric()
+age <- as.numeric(readline("Enter the age :"))
+print(age)
+
+
+
+#create two variables
+a<-"Hello"
+b<-25
+c<-3.14
+print(a)
+print(b)
+print(c)
+print(typeof(a))
+print(typeof(b))
+print(typeof(c))
+print(class(a))
+print(class(b))
+print(class(c))
+
+
+
+#store students info
+name<- "Ravi"
+marks<- 95
+#Display with labels
+cat("name :",name,"\nmarks :", marks)
+
+
+
+#TRUE & FALSE are logical values
+
+result <- TRUE
+print(result)
+print(typeof(result))
+print(class(result))
+
+
+
+#store diffrent type of values
+name <- "Riya"
+age <- 21
+marks <- 80.5
+place <- "Prayagraj"
+cat("Name :",name, "\nAge :", age , "\nMarks :", marks, "\nPlace :", place)
