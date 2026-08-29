78233ceb4fa18db51c2263c93c22ab1c9e93e7c7
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-08-29 18:48:27 +0530

    Datatypes

---
+++ datatype.R
@@ -0,0 +1,69 @@
+#used to store decimal(floating point) numbers...
+x<- 20.5
+print(x)
+class(x)
+print(typeof(x))
+
+
+#used to store text ,words and a sentences...
+x<- "Hello"
+print(x)
+class(x)
+print(typeof(x))
+
+
+#Used to store TRUE or FALSE values...
+x<-TRUE
+print(x)
+class(x)
+print(typeof(x))
+
+
+#used to store whole number and add "L" after the number to make it an integer...
+x<-20L
+print(x)
+class(x)
+print(typeof(x))
+
+
+#used to store numbers with a real and imaginary part...
+x<-3+4i
+print(x)
+class(x)
+print(typeof(x))
+
+
+
+# used to store Raw bytes (binary data)...
+x<-charToRaw("A")
+print(x)
+class(x)
+print(typeof(x))
+
+
+#NULL means no value or empty...
+x<-NULL
+print(x)
+class(x)
+print(typeof(x))
+
+
+#NA represents a missing values...
+x<-NA
+print(x)
+class(x)
+print(typeof(x))
+
+
+#NaN represents an undefined mathematical result...
+x<-0/0
+print(x)
+class(x)
+print(typeof(x))
+
+
+#Inf represents positive infinity...
+x<-10/0
+print(x)
+class(x)
+print(typeof(x))
