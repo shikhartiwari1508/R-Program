cb364c55c312eb83a09ba20181f7e62ab4e304fd
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-08-30 09:17:04 +0530

    Vector

---
+++ vector.R
@@ -0,0 +1,24 @@
+marks <- c(80,90,60,70)
+print(marks)
+
+
+
+#Length of vector
+
+marks <- c(80,90,60,70)
+print(length(marks))
+
+
+fruit <- c("Apple", "Banana","Graphs", "Mango","Orange")
+for (i in fruit){
+  print(i)
+}
+
+
+
+#While loop...
+i <-1
+while (i<=5){
+  print(i)
+  i <- i+1
+}
