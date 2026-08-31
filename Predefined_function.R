c13ff130f28e6a9839f85b55d7bb3cfe32b4e6bb
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-08-31 20:01:38 +0530

    Predefinrd Function

---
+++ Predefined_function.R
@@ -0,0 +1,19 @@
+#1. Using sum()
+
+num <- c(10,20,30,40,50)
+total <- sum(num)
+print(total)
+
+
+#2. Using mean()
+
+marks <- c(25,98,36,56,65)
+average <- mean(marks)
+print(average)
+
+
+#3. Using max() and min()
+
+marks <- c(45,67,56,92,73)
+print(min(marks))
+print(max(marks))
