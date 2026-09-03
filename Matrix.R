2aba391681fa99c85524898a613bd894d2f82e73
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-03 22:12:51 +0530

    Matrix

---
+++ Matrix.R
@@ -0,0 +1,17 @@
+#Matrix Creation
+
+m <- matrix(c(1, 2, 3, 4, 5, 6), 
+            nrow = 2, 
+            ncol = 3)
+
+print(m)
+
+
+#Matrix Addition
+
+A <- matrix(c(1, 2, 3, 4), nrow = 2)
+B <- matrix(c(5, 6, 7, 8), nrow = 2)
+
+C <- A + B
+
+print(C)
