fdefd2f4854d749991b86bf80155bce2e1fcf7ab
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-03 22:14:16 +0530

    Updated Vector Programs

---
+++ vector_.R
@@ -0,0 +1,42 @@
+#Vector Creation
+
+numbers <- c(10, 20, 30, 40, 50)
+
+print(numbers)
+
+
+#Vector Operations
+
+numbers <- c(10, 20, 30, 40, 50)
+
+print(sum(numbers))
+print(mean(numbers))
+print(max(numbers))
+print(min(numbers))
+print(length(numbers))
+
+
+
+#Vector Indexing
+
+marks <- c(45, 67, 78, 89, 92)
+
+print(marks[1])
+print(marks[3])
+print(marks[5])
+
+
+
+#Sort a Vector
+
+numbers <- c(50, 20, 80, 10, 40)
+
+print(sort(numbers))
+
+
+
+#Reverse a Vector
+
+numbers <- c(10, 20, 30, 40, 50)
+
+print(rev(numbers))
