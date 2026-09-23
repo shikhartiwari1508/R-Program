52fe7256088da8aaf28af729ff6b8439546a8408
Shikhar Tiwari <shikhar15aug@gmail.com>
2026-09-23 22:57:49 +0530

    Largest Function

--- largest_function.R
+++ largest_function.R
@@ -1,7 +1,7 @@
 largest <- function(a,b,c){
-  if(a>=b && a>=c){
+  if(a >= b && a >= c){
     return(a)
-  }else if(b>=a && b>=c){
+  }else if(b >= a && b >= c){
     return(b)
   }else{
     return(c)
